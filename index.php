5f03d9c8b3c70eabcc70694858e1aaf7fb67d2f6
Raymart <32095406+kaise12@users.noreply.github.com>
2018-01-22 02:10:49 +0800

    Guys. nag himo ko ug index.php
    check ninyo ang Index.php with style-form.css
    
    ignore nalang ng studentform nag himo nako bago. atleast my progress nata

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+	<title>Queueing System</title>
+	 <link rel="stylesheet" type="text/css" href="style-form.css">
+</head>
+
+<body>
+			<div class="header">
+				<h2 align="center">Queueing System of Notre Dame of Dadiangas University</h2>
+			</div>
+	<!--Sample Code-->
+	 <div id='container'>
+	<form name="queue" action="index.php">
+            
+            <input type="text" name="id" placeholder="Enter Your ID Number.."/><br>
+            <br>
+            
+            <input type="button" value=" 1 " onclick="queue.id.value += '1'" />
+            <input type="button" value=" 2 " onclick="queue.id.value += '2'" />
+            <input type="button" value=" 3 " onclick="queue.id.value += '3'" />
+            <br/>
+            
+            <input type="button" value=" 4 " onclick="queue.id.value += '4'" />
+            <input type="button" value=" 5 " onclick="queue.id.value += '5'" />
+            <input type="button" value=" 6 " onclick="queue.id.value += '6'" />
+
+            </br>
+          
+            <input type="button" value=" 7 " onclick="queue.id.value += '7'" />
+            <input type="button" value=" 8 " onclick="queue.id.value += '8'" />
+            <input type="button" value=" 9 " onclick="queue.id.value += '9'" />
+            </br>
+        
+            <input type="button" value=" Del " onclick="queue.id.value = ''" />
+            <input type="button" value=" 0 " onclick="queue.id.value += '0'" />
+             <input type="submit" value="Submit"/>
+            </br>
+  
+    
+          </form>
+
+
+
+
+
+
+
+	</body>
