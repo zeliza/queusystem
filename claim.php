0f10d706f8817b542959a83f2049a2e97d045b8d
zeliza <35206174+zeliza@users.noreply.github.com>
2018-02-24 22:58:06 +0800

    Add files via upload
    guyz pahelp ko , random pa ning queue number ani

---
+++ claim.php
@@ -0,0 +1,24 @@
+<html>
+
+<center> <b>
+<?php
+
+if(isset($_POST['submit'])){
+if(!empty($_POST['check_list'])) {
+$checked_count = count($_POST['check_list']);
+echo "<br> You have selected following ".$checked_count." option(s): <br/>"; 
+foreach($_POST['check_list'] as $selected) {
+echo "<p>".$selected ."</p>";
+}
+}
+else{
+echo "<b>Please Select Atleast One Option.</b>";
+}
+
+echo "Queue Number: <br>";
+echo rand(1,10);
+}
+?>
+	
+
+</html>
