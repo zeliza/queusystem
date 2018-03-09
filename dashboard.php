296f626aa3d0747e97a15771fd0aef9f0356b765
Raymart <32095406+kaise12@users.noreply.github.com>
2018-03-09 09:02:00 +0800

    Login database and SQL

---
+++ dashboard.php
@@ -0,0 +1,24 @@
+<?php
+require('db.php');
+include("auth.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dashboard - Secured Page</title>
+<!--link rel="stylesheet" href="style.css" /-->
+<link rel="stylesheet" href="css/foundation.css"/>
+<link rel="stylesheet" href="css/foundation.min.css"/>
+</head>
+<body>
+	<div class="container">
+<div class="form" style="border: 2px solid gray;">
+<p>Dashboard</p>
+<p>This is another secured page.</p>
+<p><a href="index.php">Home</a></p>
+<a href="logout.php">Logout</a>
+</div>
+</div>
+</body>
+</html>
